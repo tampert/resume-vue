e27ed2eda87bc4493ab078b1cc2be7ed2be5b517
tampert <d_soentjens@hotmail.com>
2025-08-01 16:09:02 +0200

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,9 @@
-<script setup>
-import { computed } from "vue";
-import Dashboard from "@/components/Dashboard.vue";
-import Login from "@/components/Login.vue";
-import Register from "@/components/Register.vue";
-import { useUserStore } from "@/stores/src/userStore";
-
-const userStore = useUserStore();
-
-const showLogin = computed(
-  () => !userStore.isAuthenticated && !userStore.isRegister
-);
-const showRegister = computed(
-  () => !userStore.isAuthenticated && userStore.isRegister
-);
-</script>
+<script setup></script>
 
 <template>
   <v-container>
-    <v-row>
-      <div class="wrapper">
-        <Login v-if="showLogin" />
-        <Register v-if="showRegister" />
-      </div>
-    </v-row>
-
-    <v-row>
-      <Dashboard v-if="userStore.isAuthenticated" />
-    </v-row>
+    <v-row> magic </v-row>
   </v-container>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-</style>
+<style scoped></style>
